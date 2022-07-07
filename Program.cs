36435ef692836283310c5702fd4f93eec0ffa5e2
Reight-DKC <reightultimate@gmail.com>
2022-07-07 17:17:38 -0600

    Mayor number in a list complete

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int mayornumber = 0, insertednumber;
+
+Console.WriteLine("Enter the amount of numbers");
+int totalnumber = Int32.Parse(Console.ReadLine());
+
+for (int i = 1; i <= totalnumber; i++)
+{
+    Console.WriteLine("\nInsert a number: ");
+    insertednumber = Int32.Parse(Console.ReadLine());
+
+    if(i == 1)
+    {
+        mayornumber = insertednumber;
+    }
+    else
+    {
+        if (insertednumber > mayornumber)
+        {
+            mayornumber = insertednumber;
+        }
+    }
+} 
+
+Console.WriteLine("\nMayor number: " + mayornumber);
